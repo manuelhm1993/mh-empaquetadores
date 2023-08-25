74bd9081f2bc70a8955d415f9d7361e771ccea5b
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-25 06:08:56 -0400

    Configuración del archivo webpack.config.js y compilación de assets

---
+++ Babel/webpack.config.js
@@ -0,0 +1,21 @@
+// Importaciones
+const path = require('path');
+
+// Variables
+const basePath = __dirname; // Ruta absoluta desde el directorio raíz
+const targetPath = 'dist'; // Ruta del directorio resultante
+
+// Exportaciones
+module.exports = {
+    // Desde donde empieza la aplicación
+    entry: {
+        app: './src/index.js', // Es posible crear un objeto para tener varias entradas
+    },
+    // Donde se guardan los archivos compilados
+    output: {
+        path: path.join(basePath, targetPath), // Unir la ruta actual con el directorio resultante
+        filename: 'bundle.js',
+    },
+    // Modo de funcionamiento
+    mode: 'development',
+};

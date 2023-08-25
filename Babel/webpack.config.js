b3cbe091ad130ef0c16c83b123904e05dac844da
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-25 06:43:37 -0400

    Configuración de los cargadores, module y rules para el cargador de babel

--- Babel/webpack.config.js
+++ Babel/webpack.config.js
@@ -16,6 +16,17 @@ module.exports = {
         path: path.join(basePath, targetPath), // Unir la ruta actual con el directorio resultante
         filename: 'bundle.js',
     },
+    // Puente entre una librería entre webpack y las librerías externas como sass
+    module: {
+        // Reglas que debe cumplir el archivo para entrar, algo parecido a un if
+        rules: [
+            {
+                test: /\.js$/, // Extensiones a tratar
+                use: ['babel-loader'], // Cargadores a usar
+                exclude: /node_modules/, // Excluir la carpeta node_modules
+            },
+        ],
+    },
     // Modo de funcionamiento
     mode: 'production',
 };

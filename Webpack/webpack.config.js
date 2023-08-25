f36c0a7a9efd13fed3cc111edfcbf2df060d50ab
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-25 04:22:51 -0400

    Finalizar la configuración de webpack: entrada, salida, cargadores, plugins, modo

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,6 +1,9 @@
 // Objeto para manipular rutas de directorios y archivos
 const path = require('path');
 
+// Clase para crear plantillas con los assets de webpack
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 // Exportar el config globalmente
 module.exports = {
     // Entradas: archivos a ser compilados
@@ -10,6 +13,23 @@ module.exports = {
         filename: 'app.js', // Nombre del archivo compilado
         path: path.resolve(__dirname, 'dist'), // Directorio donde se ubicará la salida
     },
+    // Cargadores: permiten procesar css, estilos y sass con javascript
+    module: {
+        // Reglas para los cargadores
+        rules: [
+            {
+                test: /\.css$/, // Expresión regular para identificar que extensiones procesar
+                use: ['style-loader', 'css-loader'], // Array para indicar la lista de cargadores a usar
+            },
+        ],
+    },
+    // Plugins: paquetes adicionales para añadir funciones especiales
+    plugin: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // Plantilla de trabajo
+            filename: './index.html' // Asset resultante con los css y js incorporados
+        })
+    ],
     // Modo de trabajo: en producción minifica el código y en desarrollo agrega ayudas para el debugging
     mode: 'development',
 };
